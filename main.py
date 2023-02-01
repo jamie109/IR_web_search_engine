5cbdd5978c94de00e972f7960fed4f4aedee9c74
fiona-aaa <2356408976@qq.com>
2023-02-01 21:17:45 +0800

    登录和用户信息保存 finish。 把登录、查询封装成两个无参函数，可能写前端的时候会简单一些？ 查询日志记录时间用户查询内容。

--- main.py
+++ main.py
@@ -13,6 +13,21 @@ import numpy as np
 import page_rank
 import datetime
 
+# 全局，不作为参数输入，搭可能前端会简单一些（我猜的）
+my_user_term = None
+
+class user_term:
+    name = None
+    age = None
+    sex = None
+    # passport = None
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        # self.passport = passport
+
+
 def content_query(file_num, query_str):
     # 加载倒排字典
     inverted_index_dic = dict()
@@ -74,22 +89,55 @@ def get_final_result(result_list):
     tf.close()
 
 
-if __name__ == '__main__':
+def log_in():
+    """
+    登录/注册
+    :return: 用户的年龄和性别
+    """
+    users_info_dic = dict()
+    user_name = input('Welcome!\nEnter user name: ')
+    # 核对是否注册过
+    try:
+        with open("dataset/users_info_dic.pkl", "rb") as tf:
+            users_info_dic = pickle.load(tf)
+    except:
+        users_info_dic = dict()
+    tf.close()
+    if user_name not in users_info_dic.keys():
+        print('Error, you have not registered yet.')
+        user_name = input('Enter the user name to register: ')
+        user_age = input('Enter your age: ')
+        user_sex = input('Enter your sex(male or female): ')
+        user_info = user_term(user_name, user_age, user_sex)
+        users_info_dic[user_name] = user_info
+        with open("dataset/users_info_dic.pkl", "wb") as t:
+            pickle.dump(users_info_dic, t)
+        t.close()
+        print('Congratulations! The registration is successful.')
+    else:
+        print('Login success, ', user_name, '!')
+    return users_info_dic[user_name]
+
+def to_query():
+    """
+    根据输入查询相关网页链接
+    :return:
+    """
     query_str = input('Please enter your query content: ')
     # 查询日志
     now_time = str(datetime.datetime.now())
-    query_log = '[' + now_time + '] query_str:' + query_str + '\n'
+    query_log = '[' + now_time + '] user_name:@' + my_user_term.name + ' query_str:' + query_str + '\n'
     with open("dataset/query_record_log.txt", "a", encoding='utf-8') as f:
         f.write(query_log)
     f.close()
     print("--------------------start query--------------------------")
     # content
     content_result_list = content_query(file_num, query_str)
-    #print(sorted(content_result_list))
+    # print(sorted(content_result_list))
     # title
     title_result_list = title_query.title_query(query_str)
-    #print(title_result_list)
-    #print(sorted(title_result_list))
+    # print(title_result_list)
+    # print(sorted(title_result_list))
 
     # pagerank
     with open("dataset/id_pagerank_dic.pkl", "rb") as tf:
@@ -107,5 +155,11 @@ if __name__ == '__main__':
     print('title_content_pagerank')
     get_final_result(title_content_pagerank)
     tf.close()
+    print("------------------------end-----------------------------")
+
+
+if __name__ == '__main__':
+    my_user_term = log_in()
+    to_query()
 
 
